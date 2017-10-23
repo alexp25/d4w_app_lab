d871267f53800cbdd305d322ef4144b1087a5adc
alexp25 <predescu.alex93@gmail.com>
2017-10-23 11:50:05 +0200

    mqtt reconnect

--- backend/modules/test/mqtt_client.py
+++ backend/modules/test/mqtt_client.py
@@ -24,6 +24,11 @@ class MQTTClient(Thread):
         client.subscribe("data/temp")
         client.subscribe("outTopic")
 
+    def on_disconnect(self, client, userdata, rc):
+        if rc != 0:
+            print("Unexpected disconnection.")
+
+
     # The callback for when a PUBLISH message is received from the server.
     def on_message(self, client, userdata, msg):
         # print(msg.topic+" "+str(msg.payload))
@@ -37,28 +42,36 @@ class MQTTClient(Thread):
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+        self.client.on_disconnect = self.on_disconnect
 
-        # self.client.connect("127.0.0.1", 1883, 60)
-        self.client.connect("192.168.1.150", 1883, 60)
+        while 1:
+            try:
+                self.client.connect("127.0.0.1", 1883, 60)
+                # self.client.connect("192.168.1.150", 1883, 60)
+            except:
+                print("Unable to connect")
+                time.sleep(5)
+                continue
 
-        # Blocking call that processes network traffic, dispatches callbacks and
-        # handles reconnecting.
-        # Other loop*() functions are available that give a threaded interface and a
-        # manual interface.
-        # self.client.loop_forever()
+            # Blocking call that processes network traffic, dispatches callbacks and
+            # handles reconnecting.
+            # Other loop*() functions are available that give a threaded interface and a
+            # manual interface.
+            # self.client.loop_forever()
 
-        rc = 0
-        while rc == 0:
-            rc = self.client.loop()
-            t1 = time.time()
+            rc = 0
+            while rc == 0:
+                rc = self.client.loop()
+                t1 = time.time()
 
-            # test
-            if t1 - self.t >= 1:
-                self.t = t1
-                self.client.publish("data/temp", 25)
-                self.client.publish("inTopic", 100)
+                # test
+                if t1 - self.t >= 1:
+                    self.t = t1
+                    self.client.publish("data/temp", 25)
+                    self.client.publish("inTopic", 100)
 
-        return rc
 
         print("EXIT MQTT")
+
+        return rc
 
