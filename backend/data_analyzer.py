2c8f3b101bf4b771e4c8a4f9e52b74a1e4687023
alexp25 <predescu.alex93@gmail.com>
2018-01-06 19:57:20 +0200

    test

--- backend/data_analyzer.py
+++ backend/data_analyzer.py
@@ -13,7 +13,9 @@ if __name__ == "__main__":
     width = 0.35  # the width of the bars
 
     case_index = 10
-    node_index = 20
+    node_index = 15
+
+
 
     lim1 = 4
     lim2 = 79
@@ -37,7 +39,34 @@ if __name__ == "__main__":
     plt.bar(ind, npmat[:, node_index], width)
     plt.gca().set_title("data for node " + str(node_index) + " for all test cases")
     plt.xlabel("test case: " + str(lim1) + " - " + str(lim2) + " initial samples")
+    plt.ylabel("average deviation")
 
+
+    plt.figure()
+
+    plt.pcolormesh(npmat)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    # Make data.
+    X = np.arange(-5, 5, 0.25)
+    Y = np.arange(-5, 5, 0.25)
+    X, Y = np.meshgrid(X, Y)
+    R = np.sqrt(X ** 2 + Y ** 2)
+    Z = np.sin(R)
+
+    # Plot the surface.
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                           linewidth=0, antialiased=False)
+
+    # Customize the z axis.
+    ax.set_zlim(-1.01, 1.01)
+    ax.zaxis.set_major_locator(LinearLocator(10))
+    ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
+
+    # Add a color bar which maps values to colors.
+    fig.colorbar(surf, shrink=0.5, aspect=5)
     plt.show()
 
 
